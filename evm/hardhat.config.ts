6e3c1020e6684475b1723250843044be81fcced1
renzocabarios <renzo.cabarios@gmail.com>
2025-01-22 13:07:49 +0800

    Redploy

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -3,6 +3,18 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      accounts: [
+        {
+          privateKey:
+            "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+          balance: "1000000000000000000000000", // 1 million ETH in wei
+        },
+        // Add more accounts as needed
+      ],
+    },
+  },
 };
 
 export default config;
