c08c6611619f9221a64d68d522047203bf0887b7
renzocabarios <renzo.cabarios@gmail.com>
2025-04-01 20:23:30 +0800

    REDEPLOY

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -5,13 +5,16 @@ const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
+        blockNumber: 1730373476,
+      },
       accounts: [
         {
           privateKey:
             "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-          balance: "1000000000000000000000000", // 1 million ETH in wei
+          balance: "1000000000000000000000000",
         },
-        // Add more accounts as needed
       ],
     },
   },
